8ac2feb1f71acefea69040385dad345589363de3
Karthik Subramanian <karthsubramanian@paypal.com>
2014-03-28 18:48:02 +0530

    little jquery selector bug in new facebook version fixed

--- reload.js
+++ reload.js
@@ -1,23 +1,27 @@
 $(function() {
+  $('.krowBookmark').remove();
   if($('._5pax').length !== 0) {
     //old facebook
-      $('._5pax').each(function() {
-        var username = strip($(this).find('a').html());
-        var content = strip($(this).find('.userContent').html()) || "";
-        var link = $(this).find('._5pcq').attr('href');  
-        content = content.substring(0, 100);
-        $(this).find('.krowBookmark').remove();
-        buildBookmarkTag($(this).find('._5pcp'), username, content, link);
-      });
+    $('._5pax').each(function() {
+      var username = strip($(this).find('a').html());
+      var content = strip($(this).find('.userContent').html()) || "";
+      var link = $(this).find('._5pcq').attr('href');  
+      content = content.substring(0, 100);
+      $(this).find('.krowBookmark').remove();
+      buildBookmarkTag($(this).find('._5pcp'), username, content, link);
+    });
   } else {
   //new facebook		
-      $('.userContentWrapper').each(function() {
+     $('.userContentWrapper').each(function() {
+      if($(this).find('.userContentWrapper').length === 0) {
         var username = strip($(this).find('._5pbw a').html());
         var content = strip($(this).find('.userContent').html()) || "";
         content = content.substring(0, 100);
         var link = $(this).find('._5pcq').attr('href');  
         $(this).find('.krowBookmark').remove();
         buildBookmarkTag($(this).find('._5vsi'), username, content, link);
-      });
+      }
+    });
+    
   }
 });

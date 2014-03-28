702f7cc67da25e1f509d1851899ed53ba7c3a818
Karthik Subramanian <karthsubramanian@paypal.com>
2014-03-28 10:05:08 +0530

    now works both on new facebook and old

--- inject.js
+++ inject.js
@@ -6,33 +6,49 @@ function strip(html) {
 }
 
 $(function() {
-  $('._5pax').each(function() {
-    var username = strip($(this).find('a').html());
-    var content = strip($(this).find('.userContent').html().substring(0, 100));
-    var link = $(this).find('._5pcq').attr('href');  
+  if($('._5pax').length !== 0) {
+    //old facebook
+      $('._5pax').each(function() {
+        var username = strip($(this).find('a').html());
+        var content = strip($(this).find('.userContent').html().substring(0, 100));
+        var link = $(this).find('._5pcq').attr('href');  
 
-    $(this).find('.krowBookmark').remove();
-    $(this).find('._5pcp').append('<a class="krowBookmark" '+
-      'data-username="'+ username + '" ' +
-      'data-content="'+ content + '" ' +
-      'data-link="'+ link + 
-      '" href="#">Bookmark</a>');
-  });
+        $(this).find('.krowBookmark').remove();
+        $(this).find('._5pcp').append('<a class="krowBookmark" '+
+          'data-username="'+ username + '" ' +
+          'data-content="'+ content + '" ' +
+          'data-link="'+ link + 
+          '" href="#">Bookmark</a>');
+      });
+  } else {
+  //new facebook		
+      $('.userContentWrapper').each(function() {
+        var username = strip($(this).find('._5pbw a').html());
+        var content = strip($(this).find('.userContent').html().substring(0, 100));
+        var link = $(this).find('._5pcq').attr('href');  
+        $(this).find('.krowBookmark').remove();
+        $(this).find('._5vsi').append('<a class="krowBookmark" '+
+          'data-username="'+ username + '" ' +
+          'data-content="'+ content + '" ' +
+          'data-link="'+ link + 
+          '" href="#">Bookmark</a>');
+        });
+  }
 
-  $('.krowBookmark').off();
-  $('.krowBookmark').click(function(){
-    var username= $(this).attr('data-username');
-    var content = $(this).attr('data-content');
-    var link = $(this).attr('data-link');
-    if(link.length > 0) {
-      if(link[0] === '/'){
-        link = window.location.origin + link;
-      }
-      chrome.runtime.sendMessage({username: username, content: content, link: link}, function(response) {
-        //console.log("success");
-      }); 
+$('.krowBookmark').off();
+$('.krowBookmark').click(function(){
+  var username= $(this).attr('data-username');
+  var content = $(this).attr('data-content');
+  var link = $(this).attr('data-link');
+  if(link.length > 0) {
+    if(link[0] === '/'){
+      link = window.location.origin + link;
     }
-  });
+    chrome.runtime.sendMessage({username: username, content: content, link: link}, function(response) {
+      //console.log("success");
+    }); 
+  }
+});
 
 });
 
